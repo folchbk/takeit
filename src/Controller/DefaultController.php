65c47d20c0f15f2e9c8cd952509e9b152a9c609c
Brian Vegara Castell <folch.bk2@gmail.com>
2019-05-09 23:04:03 +0200

    [TI#8] - Añado pantalla temporal de portada. Intento crear multiples firewalls

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,9 +12,9 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        return $this->redirect("/admin");
-//        return $this->render('default/index.html.twig', [
-//            'controller_name' => 'DefaultController',
-//        ]);
+//        return $this->redirect("/admin");
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
     }
 }
