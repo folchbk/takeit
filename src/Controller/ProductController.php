ecbedfb60202b8efc37b69d999431c88ccbfabd7
jhurtado <hurtadojose89@gmail.com>
2019-05-14 23:27:47 +0200

    [view-imgINT] -carritojs

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductController extends AbstractController
+{
+
+    private $serializer;
+    /**
+     * ProductController constructor.
+     */
+    public function __construct()
+    {
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $this->serializer = new Serializer($normalizers, $encoders);
+    }
+
+
+    /**
+     * @Route("/getProduct/{id}", name="getProduct")
+     */
+    public function getProduct($id)
+    {
+        $em = $this->getDoctrine()->getManager()->getRepository(Product::class);
+        $product = $em->findOneBy(array('id'=>$id));
+
+        $filteredProduct = array(
+           'id' => $product->getId(),
+           'name' => $product->getName(),
+           'description' => $product->getDescription(),
+           'price' => $product->getPrice()
+        );
+
+        $response = new Response(json_encode($filteredProduct));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+}
