cab825693adddc26c2f56ed8cebed7a946630f6c
Brian Bartes Bernal <bbartes@sumatd.com>
2019-05-15 18:13:09 +0200

    [TI#8] - Menu en marcha, falta asignar producto

---
+++ src/Form/MenuType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Menu;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MenuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('menuProducts', CollectionType::class, array(
+                    'label' => '',
+                    'entry_type' => MenuProductType::class,
+                    'entry_options' => array('label' => false),
+                    'allow_add' => true,
+                    'allow_delete' => true,
+                    'by_reference' => true,
+                    'attr' => array(
+                        'class' => 'form-control'
+                    )
+                )
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Menu::class,
+        ]);
+    }
+}
