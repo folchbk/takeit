2acd20260a7cf31b7970305dbc29c4870fa6aed5
Brian Bartés Bernal <folch.bk2@gmail.com>
2019-05-16 20:42:08 +0200

    [TI#10] - Testeo en marcga

--- src/Admin/TableAdmin.php
+++ src/Admin/TableAdmin.php
@@ -14,18 +14,6 @@ use Sonata\AdminBundle\Route\RouteCollection;
 
 final class TableAdmin extends AbstractAdmin
 {
-
-    protected $parentAssociationMapping = 'table_id';
-
-    protected function configureRoutes(RouteCollection $collection)
-    {
-        if ($this->isChild()) {
-            return;
-        }
-
-        // This is the route configuration as a parent
-        $collection->clear();
-    }
         protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
     {
         $datagridMapper
