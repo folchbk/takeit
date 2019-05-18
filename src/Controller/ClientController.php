54852a89861a0198c92c30d1597347902407f472
Brian Vegara Castell <folch.bk2@gmail.com>
2019-05-18 12:01:32 +0200

    [TI#10] - Repository de invoices y de clients OK

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/client")
+ * @Route("/backoffice/client")
  */
 class ClientController extends AbstractController
 {
