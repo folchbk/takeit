a83d8c3b797045f58905f718e2f2cfd359a3cb7a
Brian Vegara Castell <folch.bk2@gmail.com>
2019-05-19 12:37:45 +0200

    [TI#11] - Order OK

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,21 +2,25 @@
 
 namespace App\Controller;
 
-use App\Entity\Deal;
 use App\Entity\Order;
-use App\Entity\OrderProduct;
-use App\Entity\Product;
+use App\Form\OrderType;
 use App\Repository\OrderRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Deal;
+use App\Entity\OrderProduct;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
+/**
+ * @Route("/")
+ */
 class OrderController extends AbstractController
 {
     private $serializer;
@@ -29,6 +33,91 @@ class OrderController extends AbstractController
         $normalizers = [new ObjectNormalizer()];
         $this->serializer = new Serializer($normalizers, $encoders);
     }
+
+    /**
+     * @Route("/backoffice/order/", name="order_index", methods={"GET"})
+     */
+    public function index(OrderRepository $orderRepository): Response
+    {
+        return $this->render('order/index.html.twig', [
+            'orders' => $orderRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/backoffice/order/new", name="order_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $order = new Order();
+        $form = $this->createForm(OrderType::class, $order);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            foreach ($order->getOrderProducts() as $orderProduct) {
+                $orderProduct->setOrderObject($order);
+            }
+
+            $entityManager->persist($order);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('order_index');
+        }
+
+        return $this->render('order/new.html.twig', [
+            'order' => $order,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/backoffice/order/{id}", name="order_show", methods={"GET"})
+     */
+    public function show(Order $order): Response
+    {
+        return $this->render('order/show.html.twig', [
+            'order' => $order,
+        ]);
+    }
+
+    /**
+     * @Route("/backoffice/order/{id}/edit", name="order_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Order $order): Response
+    {
+        $form = $this->createForm(OrderType::class, $order);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('order_index', [
+                'id' => $order->getId(),
+            ]);
+        }
+
+        return $this->render('order/edit.html.twig', [
+            'order' => $order,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/backoffice/order/{id}", name="order_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Order $order): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$order->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($order);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('order_index');
+    }
+
     /**
      * @Route("/getActiveOrders", name="order")
      */
@@ -77,24 +166,22 @@ class OrderController extends AbstractController
     public function generateOrder() {
 
         $em = $this->getDoctrine()->getManager();
-      $productos = get_object_vars(json_decode($_POST["carrito"]));
-      $order = new Order();
-      $order->setStatus($_POST['status']);
-      $em->persist($order);
-      $em->flush();
-      foreach ($productos["_productos"] as $producto) {
-          $producto = get_object_vars($producto);
-          $productReal = $em->getRepository(Product::class)->findOneBy(array('id' => $producto['_id']));
-          $orderProduct = new OrderProduct();
-          $orderProduct->setOrderObject($order);
-          $orderProduct->setProduct($productReal);
-          $orderProduct->setQuantity($producto['_quantity']);
-          $em->persist($orderProduct);
-          $em->flush();
-      }
-      return new Response('ok');
+        $productos = get_object_vars(json_decode($_POST["carrito"]));
+        $order = new Order();
+        $order->setStatus($_POST['status']);
+        $em->persist($order);
+        $em->flush();
+        foreach ($productos["_productos"] as $producto) {
+            $producto = get_object_vars($producto);
+            $productReal = $em->getRepository(Product::class)->findOneBy(array('id' => $producto['_id']));
+            $orderProduct = new OrderProduct();
+            $orderProduct->setOrderObject($order);
+            $orderProduct->setProduct($productReal);
+            $orderProduct->setQuantity($producto['_quantity']);
+            $em->persist($orderProduct);
+            $em->flush();
+        }
+        return new Response('ok');
 
     }
-
-
 }
